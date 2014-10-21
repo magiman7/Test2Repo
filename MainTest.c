bad0b6f2160ffda2cd0590b0b1f417b6476767fb
Paul Hummel <phummel@latech.edu>
2014-10-20 23:00:18 -0500

    added coolest feature
    This makes this app much cooler

--- MainTest.c
+++ MainTest.c
@@ -5,6 +5,8 @@ void main ()
     xil_printf("Circuits are wonderful!\r\n");
     
     xil_prntf("Dr. Hummel is the bestest teacher EVER!!!\r\n");
+    
+    xil_printf("Coolest feature is this right here");
 
 }
     
