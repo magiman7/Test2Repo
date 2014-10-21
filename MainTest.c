56586b4032057fd41644f5235eca9336ec1b5cde
Paul Hummel <phummel@latech.edu>
2014-10-20 22:48:32 -0500

    Update MainTest.c

--- MainTest.c
+++ MainTest.c
@@ -4,7 +4,7 @@ void main ()
 {
     xil_printf("Circuits are wonderful!\r\n");
     
-    xil_prntf("Dr. Hummel is the bestest teacher EVER!!!\r\n");
+    xil_printf("Dr. Hummel is the bestest teacher EVER!!!\r\n");
 
 }
     
